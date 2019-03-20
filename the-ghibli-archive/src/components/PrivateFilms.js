15ff89ff7a48bda41ddd2f9d3820d8405610ada3
Tyrone Oggen <Tyrone.Oggen@britehouse.co.za>
2019-03-20 18:31:05 +0200

    authenticated data display

--- the-ghibli-archive/src/components/PrivateFilms.js
+++ the-ghibli-archive/src/components/PrivateFilms.js
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PrivateFilms extends Component {
-  state = {
-    message: ""
-  };
-
-  componentDidMount() {
-    fetch("/private", {
-      headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
-    })
-      .then(response => {
-        if (response.ok) return response.json();
-        throw new Error("Network response was not ok.");
-      })
-      .then(response => this.setState({ message: response.message }))
-      .catch(error => this.setState({ message: error.message }));
-  }
-
   render() {
-    return <div>{this.state.message}</div>;
+    const filmData = axios.get("https://ghibliapi.herokuapp.com/films");
+
+    console.log(filmData.data);
+    return <div />;
   }
 }
 
